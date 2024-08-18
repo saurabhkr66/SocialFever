a64c05f94e7ed39a8dd67ba62785d21ab4447b03
saurabhkr66 <sauravkum420@gmail.com>
2024-08-19 02:11:52 +0530

    changes in app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,7 @@ function App() {
  
 const mode=useSelector((state)=>state.mode);
 const theme=useMemo(()=>createTheme(themeSettings(mode)),[mode]);
+const isAuth=Boolean(useSelector((state)=>state.token));
   return (
   
       <div className='app'>
@@ -22,7 +23,7 @@ const theme=useMemo(()=>createTheme(themeSettings(mode)),[mode]);
         <Routes>
           <Route path='/' element={<LoginPage/>} />
           <Route path='/profile/:userId' element={<ProfilePage />} />
-          <Route path='/home' element={<HomePage />} />
+          <Route path='/home' element={isAuth?<HomePage />:<Navigate to ="/"/>} />
         </Routes>
         </ThemeProvider>
         </BrowserRouter>
